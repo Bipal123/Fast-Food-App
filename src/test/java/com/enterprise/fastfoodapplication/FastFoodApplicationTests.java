b3d1ec5342acbefe988a3dbfa98fc51d88d3fcdd
ajmorgs <ajmorgs@gmail.com>
2021-10-10 11:08:45 -0400

    Add unit test for create/update of FoodItem

--- src/test/java/com/enterprise/fastfoodapplication/FastFoodApplicationTests.java
+++ src/test/java/com/enterprise/fastfoodapplication/FastFoodApplicationTests.java
@@ -1,13 +1,45 @@
 package com.enterprise.fastfoodapplication;
 
+import com.enterprise.fastfoodapplication.dto.Food;
+import com.enterprise.fastfoodapplication.service.IFoodService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class FastFoodApplicationTests {
+
+    private IFoodService foodService;
+    private  Food food = new Food();
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void updateFoodDataWithChickenBurger(){
+        givenFoodDataIsAvailable();
+        whenUserSpecifiesCategorySnacks();
+        whenUserSpecifiesPriceTwelveNinetyNine();
+        thenAddChickenBurgerToFoodData();
+    }
+
+    private void givenFoodDataIsAvailable() {
+        food.setFoodName("ChickenBurger");
+        food.setFoodCategory("Snacks");
+        food.setFoodPrice(12.99);
+    }
+
+    private void whenUserSpecifiesCategorySnacks() {
+        assertEquals("Snacks",food.getFoodCategory());
+    }
+
+    private void whenUserSpecifiesPriceTwelveNinetyNine() {
+        assertEquals(12.99,food.getFoodPrice());
+    }
+
+    private void thenAddChickenBurgerToFoodData() {
+
+        foodService.createFoodItem(food);
+    }
 }
