e64c00fdfa40f79ed5699af324d2378e44f25101
Bipal Goyal <goyalbl@mail.uc.edu>
2021-10-10 20:08:37 +0545

    Added 2 Unit Tests

---
+++ src/main/java/com/enterprise/fastfoodapplication/service/FoodServiceStub.java
@@ -0,0 +1,49 @@
+package com.enterprise.fastfoodapplication.service;
+
+import com.enterprise.fastfoodapplication.dao.IFoodDao;
+import com.enterprise.fastfoodapplication.dto.Food;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class FoodServiceStub implements IFoodService {
+
+    private IFoodDao foodDao;
+
+    public FoodServiceStub() {
+
+    }
+
+    public FoodServiceStub(IFoodDao foodDao) {
+        this.foodDao = foodDao;
+    }
+
+    @Override
+    public Food getFoodItemById(int id) {
+        Food food = new Food();
+        food.setFoodId("9");
+        food.setFoodName("Italian Pizza");
+        return food;
+    }
+
+    @Override
+    public Map<String, Food> getAllFoodItems() {
+        return null;
+    }
+
+    @Override
+    public Food createFoodItem(Food food) throws  Exception{
+        return foodDao.createFoodItem(food);
+    }
+
+    @Override
+    public void updateFoodItem(int id) {
+
+    }
+
+    @Override
+    public void removeFoodItem(String id) {
+
+    }
+}
--- src/test/java/com/enterprise/fastfoodapplication/FastFoodApplicationTests.java
+++ src/test/java/com/enterprise/fastfoodapplication/FastFoodApplicationTests.java
@@ -1,13 +1,70 @@
 package com.enterprise.fastfoodapplication;
 
+import com.enterprise.fastfoodapplication.dao.IFoodDao;
+import com.enterprise.fastfoodapplication.dto.Food;
+import com.enterprise.fastfoodapplication.service.FoodServiceStub;
+import com.enterprise.fastfoodapplication.service.IFoodService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class FastFoodApplicationTests {
 
+    private IFoodService foodService;
+    private Food food = new Food();
+
+    @MockBean
+    private IFoodDao foodDao;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void getFoodItemById_returnsItalianPizzaForID9() throws Exception {
+        givenFoodsDataAreAvailable();
+        whenSearchFoodWithID9();
+        thenReturnOneItalianPizzaFoodForID9();
+    }
+
+    private void givenFoodsDataAreAvailable() throws Exception {
+        Mockito.when(foodDao.createFoodItem(food)).thenReturn(food);
+        foodService = new FoodServiceStub(foodDao);
+    }
+
+    private void whenSearchFoodWithID9() {
+        food = foodService.getFoodItemById(9);
+    }
+
+    private void thenReturnOneItalianPizzaFoodForID9() {
+        String foodName = food.getFoodName();
+        assertEquals("Italian Pizza", foodName);
+    }
+
+    @Test
+    void createFood_validateReturnFoodWithFoodNameFoodCategoryAndFoodPrice() throws Exception {
+        givenFoodsDataAreAvailable();
+        whenUserCreatesANewFoodAndSaves();
+        thenCreateNewFoodRecordAndReturnIt();
+    }
+
+    private void whenUserCreatesANewFoodAndSaves() {
+        food.setFoodName("Chicken Burger");
+        food.setFoodCategory("Snacks");
+        food.setFoodPrice(12.99);
+    }
+
+    private void thenCreateNewFoodRecordAndReturnIt() throws  Exception{
+        Food createdFood = foodService.createFoodItem(food);
+        assertEquals(food, createdFood);
+        verify(foodDao, atLeastOnce()).createFoodItem(food);
     }
 
 }
