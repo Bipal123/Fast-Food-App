90863e48abbf8294eca521b1da68cc593290ab71
Jubn-yan <jubn@mail.uc.edu>
2021-11-02 21:20:50 +0800

    update category controller.java

--- src/main/java/com/enterprise/fastfoodapplication/controllers/CategoryController.java
+++ src/main/java/com/enterprise/fastfoodapplication/controllers/CategoryController.java
@@ -1,13 +1,24 @@
 package com.enterprise.fastfoodapplication.controllers;
+import com.enterprise.fastfoodapplication.dto.Food;
+import com.enterprise.fastfoodapplication.service.IFoodService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 
 
 public class CategoryController {
+    @Autowired
+    private IFoodService iFoodservice;
+
     @GetMapping("/category")
-    public String getCategory(){
+    public String getCategory(Model model) throws Exception {
+        List<Food> foodList = iFoodservice.getAllFoodItems();
+        model.addAttribute("Food",foodList);
         return "Category";
     }
 }
