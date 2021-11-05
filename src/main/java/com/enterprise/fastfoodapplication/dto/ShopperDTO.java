e649ec5c231dc88b13b269a6d28d2f036f3f4c2c
bentonjc <bentonjc@mail.uc.edu>
2021-11-05 18:34:52 -0400

    Created a ShopperDTO as a means to store user information. Given more than one person my be using this applciaiton - it is important to create a data object for this. This can also help simplify other class structures.

---
+++ src/main/java/com/enterprise/fastfoodapplication/dto/ShopperDTO.java
@@ -0,0 +1,14 @@
+package com.enterprise.fastfoodapplication.dto;
+
+
+import lombok.Data;
+
+public @Data
+class ShopperDTO {
+
+    private int shopperID;
+    private String firstName;
+    private String lastName;
+
+
+}
